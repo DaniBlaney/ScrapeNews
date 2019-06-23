eb2a22b7dccbfac1e5b8df1e7330e6732130d2e6
DaniBlaney <daniblaney@gmail.com>
2019-06-23 19:58:08 -0400

    fixed undefined, now get title x2

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.foxnews.com/").then(function(response) {
+  axios.get("https://www.foxnews.com/us").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
